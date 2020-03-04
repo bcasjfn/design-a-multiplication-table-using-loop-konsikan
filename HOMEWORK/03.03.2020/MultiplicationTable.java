ce20c86a52f02cafdcbfbc04806dd705230e13b1
konsikan <konsikankj0104@gmail.com>
2020-03-04 00:20:35 -0800

    first commit

---
+++ HOMEWORK/03.03.2020/MultiplicationTable.java
@@ -0,0 +1,9 @@
+public class MultiplicationTable{
+	public static void main(String args[]){
+		int num=8;
+		System.out.println("| MULTIPLICATION TABLE FOR |- "+num);
+		for(int i=1; i<=12; i++){
+					System.out.println(i+" x "+num+" = "+i*num);
+		}	
+	}
+}
